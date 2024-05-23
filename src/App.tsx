8f43478fc9fc26dca8a8fc9fc4c1b0f8a5e52379
Maverick Seneris <maverickseneris@gmail.com>
2024-05-24 07:47:51 +0800

    feat: integrated openweather one call api to fetch weather data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
+import {useState, useEffect} from "react";
+import axios from 'axios'
 import "./App.css";
 
 function App() {
-  return <h1 className="text-4xl font-bold underline text-blue-500">Hello world!</h1>;
+  const [weatherData, setWeatherData] = useState(null)
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+useEffect(() => {
+  const fetchWeather = async () => {
+    try {
+      const apiKey = "fd994ec80d186de6dc97b9fd5ef8aac2"
+      const lat = "14.5948914"
+      const long = "120.9782618"
+      const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`
+
+      const response = await axios.get(url)
+      setWeatherData(response.data)
+    } catch (error) {
+      setError(error.message)
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  fetchWeather()
+}, [])
+
+console.log(weatherData)
+
+if (loading) <div>Loading...</div>
+if (error) <div>Error:{error}</div>
+  return (
+    <div className="h-screen bg-gradient-to-r from-rainbow">
+      
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        rainbow:
+          "linear-gradient(45deg, #ff0000, #ff8c00, #ff0, #008000, #00f, #4b0082, #8a2be2)",
+      }),
+    },
   },
   plugins: [],
-}
+};
